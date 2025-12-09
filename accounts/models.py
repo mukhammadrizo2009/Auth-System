89f0f0890a8496acb21eae85a0549fbc445468ab
mukhammadrizo2009 <muhammadrizomirzaev671@gmail.com>
2025-12-09 16:04:55 +0500

    Models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+class CustomUser(AbstractBaseUser):
+    ROLES = [
+        ['ADMIN', 'Admin'],
+        ['USER', 'User'],
+        ['MANAGER', 'Manager'],
+    ]
+    
+    role = models.CharField(
+        choices=ROLES,
+        default='USER'
+    )
+    
+    @property
+    def is_admin(self):
+        return self.role == 'ADMIN'
+    
+    @property
+    def is_user(self):
+        return self.role == 'USER'
+    
+    @property
+    def is_manager(self): 
+        return self.role == 'MANAGER'
