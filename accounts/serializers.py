1f509d6960bf450f15222ca22619f327360e0c48
mukhammadrizo2009 <muhammadrizomirzaev671@gmail.com>
2025-12-09 16:35:44 +0500

    Serializers

---
+++ accounts/serializers.py
@@ -0,0 +1,59 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+User = get_user_model()
+
+class UserSerializers(serializers.ModelSerializer):
+    
+    class Meta:
+        model = User
+        exclude = ['groups','user_permissions','is_staff','is_superuser','last_login']
+        
+class RegisterSerializers(serializers.ModelSerializer):
+    confirm = serializers.CharField(max_lenght=128)
+    
+    class Meta:
+        model = User
+        fields = ['username','password','confirm','email','first_name','last_name']
+        
+    def validate(self, attrs):
+        if attrs['password'] != attrs['confirm']:
+            raise serializers.ValidationError("Passwordlar bir-biriga mos emas!")
+        
+        return super().validate(attrs)
+    
+    def create(self, validated_data):
+        validated_data.pop('confirm')
+        password = validated_data.pop('password')
+        
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        
+        return user
+    
+class LoginSerializer(serializers.Serializer):
+        username = serializers.CharField(max_length=150)
+        password = serializers.CharField(max_length=128)
+        
+class ProfileSerializers(serializers.ModelSerializer):
+        
+        class Meta:
+            model = User
+            exclude = ['password','role','groups','user_permissions','is_staff','is_superuser']
+            extra_kwargs = {
+                'username': {
+                    'required': False
+                }
+            }
+            
+class PasswordChangeSerializer(serializers.Serializer):
+    password = serializers.CharField(max_length=128)
+    new_password = serializers.CharField(max_length=128)
+    confirm = serializers.CharField(max_length=128)
+    
+    def validate(self, attrs):
+        if attrs['new_password'] != attrs['confirm']:
+            raise serializers.ValidationError('Passwordlar bir-biriga mos emas!')
+        
+        return super().validate(attrs)
