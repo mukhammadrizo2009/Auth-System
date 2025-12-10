0b1fd59f9b46934a4acede4ea7ab82b50ee066a3
mukhammadrizo2009 <muhammadrizomirzaev671@gmail.com>
2025-12-10 14:31:02 +0500

    Urls

---
+++ accounts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import RegisterView, LoginView, LogoutView, ProfileView, PasswordChangeView, AdminPanelView
+
+urlpatterns = [
+    path('auth/register/', RegisterView.as_view()),
+    path('auth/login/', LoginView.as_view()),
+    path('auth/logout/', LogoutView.as_view()),
+    path('auth/profile/', ProfileView.as_view()),
+    path('auth/change-password/', PasswordChangeView.as_view()),
+
+    path('admin-panel/', AdminPanelView.as_view())
+]
